0ac1036a25b47bcac016900c6e751071e7146667
Ulrich Petri <ulo@ulo.pe>
2023-08-01 16:05:46 +0200

    Add shutter init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import * as Shutter from "@shutter-network/shutter-crypto";
+import Shutter from "@shutter-network/shutter-crypto";
 import * as Sentry from "@sentry/node";
 
-console.log("before");
+console.log("before sentry");
 Sentry.init();
-console.log("after");
+console.log("after sentry / before shutter");
+await Shutter.init();
+console.log("after shutter");
